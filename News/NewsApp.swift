ec6b270297da7290982fcac86faf6d2f7be2c363
yabby <yabby1997@gmail.com>
2021-07-08 23:26:52 +0900

    initial commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Seunghun Yang on 2021/07/08.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
