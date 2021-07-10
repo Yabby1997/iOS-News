8e44c0b4ab43d42040dd11e599c7e7d9ac6af553
yabby <yabby1997@gmail.com>
2021-07-10 22:24:07 +0900

    NewsViewModel 추가

---
+++ News/ViewModels/NewsViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  NewsViewModel.swift
+//  News
+//
+//  Created by Seunghun Yang on 2021/07/09.
+//
+
+import Foundation
+import Combine
+
+protocol NewsViewModel {
+    func getArticles()
+}
+
+class NewsViewModelImplementation: ObservableObject, NewsViewModel {
+    
+    private let service: NewsService
+    private(set) var articles = [Article]()
+    private var cancellables = Set<AnyCancellable>()
+    
+    @Published private(set) var state: ResultState = .loading
+    
+    init(service: NewsService) {
+        self.service = service
+    }
+    
+    func getArticles() {
+        let cancellable = service.request(from: .getNews)
+            .sink { response in
+                switch(response) {
+                case .finished:
+                    self.state = .success(content: self.articles)
+                case .failure(let error):
+                    self.state = .failed(error: error)
+                }
+            } receiveValue: { response in
+                self.articles = response.articles
+            }
+        self.cancellables.insert(cancellable)
+    }
+}
