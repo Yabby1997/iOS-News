c090a6dc7e411b01afad85f14b72fb8bbb0d6677
yabby <yabby1997@gmail.com>
2021-07-11 18:13:26 +0900

    HomeView 구성

--- News/Views/HomeView.swift
+++ News/Views/HomeView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @StateObject var viewModel: NewsViewModelImplementation = NewsViewModelImplementation(service: NewsServiceImplementation())
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Group {
+            switch viewModel.state {
+            case .loading:
+                ProgressView()
+            case .failed(let error):
+                ErrorView(error: error, retryHandler: viewModel.getArticles)
+            case .success(let articles):
+                NavigationView {
+                    List(articles) { item in
+                        ArticleView(article: item)
+                    }
+                    .navigationTitle(Text("News"))
+                }
+            }
+        }.onAppear(perform: viewModel.getArticles)
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .previewDevice("iPhone 12")
     }
 }
