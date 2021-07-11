fa80211c7671724256d3b0f7cef6e017f1594eee
yabby <yabby1997@gmail.com>
2021-07-11 18:04:01 +0900

    ErrorView 구현

---
+++ News/Views/ErrorView.swift
@@ -0,0 +1,55 @@
+//
+//  ErrorView.swift
+//  News
+//
+//  Created by Seunghun Yang on 2021/07/11.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    
+    typealias RetryHandler = () -> Void
+    
+    let retryHandler: RetryHandler
+    let error: Error
+    
+    init(error: Error, retryHandler: @escaping RetryHandler) {
+        self.error = error
+        self.retryHandler = retryHandler
+    }
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "exclamationmark.icloud.fill")
+                .foregroundColor(.gray)
+                .font(.system(size: 50, weight: .heavy))
+                .padding(.bottom, 4)
+            Text("Ooops!")
+                .font(.system(size: 30, weight: .heavy))
+            Text(error.localizedDescription)
+                .foregroundColor(.gray)
+                .font(.system(size: 15))
+                .multilineTextAlignment(.center)
+                .padding(.vertical, 4)
+            Button(action: {
+                retryHandler()
+            }, label: {
+                Text("Retry")
+            })
+            .padding(.vertical, 12)
+            .padding(.horizontal, 30)
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .font(.system(size: 15, weight: .heavy))
+            .cornerRadius(10)
+        }
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ErrorView(error: APIError.decodingError) {}
+            .previewLayout(.sizeThatFits)
+    }
+}
